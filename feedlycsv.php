c247be45fa2c1b6b4c593ee4cf2ad0536c05399e
Gabriel Albino <enggabrielalbino@gmail.com>
2018-03-07 20:32:03 -0300

    Update feedlycsv.php

--- feedlycsv.php
+++ feedlycsv.php
@@ -3,7 +3,7 @@
 	{ 
 		$filteredPortals = array();
 		foreach($portals as $portal){
-			$index = strpos($portal, ">") + 1; #localiza onde o nome do portal comeÁa
+			$index = strpos($portal, ">") + 1; #localiza onde o nome do portal come√ßa
 			$filteredPortal = substr($portal, $index, strlen($portal)-$index-4); #pega o nome do portal
 			array_push($filteredPortals, $filteredPortal); #adiciona no vetor
 		} 
@@ -14,8 +14,8 @@
 	{ 
 		$filteredTitles = array();
 		foreach($titles as $title){
-			$index = strpos($title, ">") + 1; #localiza onde o nome do portal comeÁa
-			$filteredTitle = substr($title, $index, strlen($title)-$index); #pega o nome do portal
+			$index = strpos($title, ">") + 1; #localiza onde o nome do titulo come√ßa
+			$filteredTitle = substr($title, $index, strlen($title)-$index); #pega o nome do titulo
 			array_push($filteredTitles, $filteredTitle); #adiciona no vetor
 		} 
 		return $filteredTitles; #retorna o vetor;
@@ -25,10 +25,10 @@
 	{ 
 		$filteredEngagements = array();
 		foreach($engagements as $engagement){
-			$index = strpos($engagement, ">") + 1; #localiza onde o nome do portal comeÁa
-			$filteredEngagement = substr($engagement, $index, strlen($engagement)-$index-7); #pega o nome do portal
+			$index = strpos($engagement, ">") + 1; #localiza onde o numero dos cliques come√ßa
+			$filteredEngagement = substr($engagement, $index, strlen($engagement)-$index-7); #pega o numero dos cliques
 			echo "";
-			if(strpos($filteredEngagement, "+") !== false){
+			if(strpos($filteredEngagement, "+") !== false){ #retira o caractere '+'
 				$filteredEngagement[strpos($filteredEngagement, "+")] = '^';
 			}
 			else{
@@ -40,10 +40,10 @@
 	} 
 
 	$content = file_get_contents($argv[1]);
-	preg_match_all('/<div class=\"meta\-column\">(.*?)<\/div>/s',$content,$news_portal); #aqui a gente t· pegando o html do portal da noticia
+	preg_match_all('/<div class=\"meta\-column\">(.*?)<\/div>/s',$content,$news_portal); #aqui a gente t√° pegando o html do portal da noticia
 	preg_match_all("'a class=\"title\"(.*?)</a>'si",$content,$news_title); 
-		#aqui  gente t· pegando o html do titulo da noticia
-	preg_match_all('/<div class=\"engagement\-container\">(.*?)<\/div>/s',$content,$news_engagement); #aqui a gente t· pegando o html dos acessos da noticia
+		#aqui  gente t√° pegando o html do titulo da noticia
+	preg_match_all('/<div class=\"engagement\-container\">(.*?)<\/div>/s',$content,$news_engagement); #aqui a gente t√° pegando o html dos acessos da noticia
 	$portals = filterPortal($news_portal[1]); #retirando as tags html
 	$titles = filterTitle($news_title[1]); #retirando as tags html
 	$engagements = filterEngagement($news_engagement[1]); #retirando as tags html
@@ -52,12 +52,12 @@
 		array_push($csvLines, "\"$portals[$i]\",\"$titles[$i]\",\"$engagements[$i]\"\n") ; #armazenando nossos dados no vetor
 	}
 	$file = fopen("results.csv", "w+"); #abrindo o arquivo pra armazenar os dados
-	fwrite($file, "Portal,Titulo,Acesso\n"); #criando cabeÁalho do csv
+	fwrite($file, "Portal,Titulo,Acesso\n"); #criando cabe√ßalho do csv
 	$contador = 1;
 	foreach ($csvLines as $csv) { #iterando entre as linhas
-		fwrite($file, $csv); #para cada linha adicinar ela no arquivo de saÌda
+		fwrite($file, $csv); #para cada linha adicinar ela no arquivo de sa√≠da
 		echo "$contador - $csv"; #debug
-		$contador++; #indicando qual noticia È (para comparaÁ„o)
+		$contador++; #indicando qual noticia √© (para compara√ß√£o)
 	}
 	fclose($file); #fechando o arquivo
 ?>
